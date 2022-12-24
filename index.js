9c7b303f7a98c2473d40df955aede42bebd25362
Kelly <fabianachara@gmail.com>
2022-12-24 14:45:47 -0500

    add city search functionality

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
 import { readInput, inquireMenu, pause } from "./utils/inquirer.js";
+import Searchers from "./models/searches.js";
 
 const main = async () => {
   let opt;
 
   do {
     opt = await inquireMenu();
-    console.log({opt});
+    console.log({ opt });
+const cities = new Searchers
+    switch (opt) {
+      case 1:
+        const city = await readInput("City: ");
+        await cities.fetchCity( city )
+            
+        
+        console.log("\nCity info\n".green);
+        console.log(` City: `);
+        console.log(` Lat:`);
+        console.log(` Lng:`);
+        console.log(` Temp:`);
+        console.log(` Min Temp:`);
+        console.log(` Max Temp:`);
 
-    // switch (opt) {
-    //   case 1:
-    //     const city = await readInput("choose a city");
-    //     console.log( city );
+        break;
 
-    //     break;
+      case 2:
+        console.log("the historial");
 
-    //   case 2:
-    //     console.log("the historial");
+        break;
+    }
 
-    //     break;
-    // }
-
-  if ( opt !== 0) { await pause()}
+    if (opt !== 0) {
+      await pause();
+    }
   } while (opt);
 };
 
--- models/searches.js
+++ models/searches.js
@@ -1,15 +1,32 @@
+import axios from "axios";
 export default class Searchers {
+  historial = ["Cali", "Rome", "Dubai", "Rio de Janeiro"];
 
-    historial = ['Cali', 'Rome', 'Dubai', 'Rio de Janeiro']
+  get paramsMapbox() {
+    return {
+      access_token: process.env.MAPBOX_KEY,
+      limit: 5,
+      language: "en",
+    };
+  }
+  constructor() {}
 
-    constructor() {
+  async fetchCity(place = "") {
+    try {
+      const instace = axios.create({
+        baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${place}.json?`,
+        params: this.paramsMapbox,
+      });
 
+      const { data } = await instace.get();
+
+      console.log(data.features);
+
+      return [];
+    } catch (err) {
+      return [];
     }
 
-    async city( place = '' ) {
-        // maket http request
-
-        console.log(place)
-        
-    }
+    return [];
+  }
 }
